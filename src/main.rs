f05d51b833a0cc092c9413dbb867508d80a8e49a
Taizeng Wu <wutaizeng@gmail.com>
2022-08-12 17:37:10 +0800

    feat: implement http proxy

---
+++ src/main.rs
@@ -0,0 +1,102 @@
+//! Run with
+//!
+//! ```not_rust
+//! $ cargo run
+//! ```
+//!
+//! In another terminal:
+//!
+//! ```not_rust
+//! $ curl -v -x "127.0.0.1:3000" https://tokio.rs
+//! ```
+//!
+//! Example is based on <https://github.com/tokio-rs/axum/blob/main/examples/http-proxy/src/main.rs>
+
+use axum::{
+    body::{self, Body},
+    http::{Method, Request, StatusCode},
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use hyper::upgrade::Upgraded;
+use tokio::net::TcpStream;
+use tower::{make::Shared, ServiceExt};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG")
+                .unwrap_or_else(|_| "http_proxy=trace,tower_http=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let router = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let service = tower::service_fn(move |req: Request<Body>| {
+        let router = router.clone();
+        async move {
+            if req.method() == Method::CONNECT {
+                proxy(req).await
+            } else {
+                router.oneshot(req).await.map_err(|err| match err {})
+            }
+        }
+    });
+
+    let addr = std::env::var("HTTP_PROXY_LISTEN")
+        .unwrap_or("0.0.0.0:3000".to_string())
+        .parse()
+        .unwrap();
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .http1_preserve_header_case(true)
+        .http1_title_case_headers(true)
+        .serve(Shared::new(service))
+        .await
+        .unwrap();
+}
+
+async fn proxy(req: Request<Body>) -> Result<Response, hyper::Error> {
+    tracing::trace!(?req);
+
+    if let Some(host_addr) = req.uri().authority().map(|auth| auth.to_string()) {
+        tokio::task::spawn(async move {
+            match hyper::upgrade::on(req).await {
+                Ok(upgraded) => {
+                    if let Err(e) = tunnel(upgraded, host_addr).await {
+                        tracing::warn!("server io error: {}", e);
+                    };
+                }
+                Err(e) => tracing::warn!("upgrade error: {}", e),
+            }
+        });
+
+        Ok(Response::new(body::boxed(body::Empty::new())))
+    } else {
+        tracing::warn!("CONNECT host is not socket addr: {:?}", req.uri());
+        Ok((
+            StatusCode::BAD_REQUEST,
+            "CONNECT must be to a socket address",
+        )
+            .into_response())
+    }
+}
+
+async fn tunnel(mut upgraded: Upgraded, addr: String) -> std::io::Result<()> {
+    let mut server = TcpStream::connect(addr).await?;
+
+    let (from_client, from_server) =
+        tokio::io::copy_bidirectional(&mut upgraded, &mut server).await?;
+
+    tracing::debug!(
+        "client wrote {} bytes and received {} bytes",
+        from_client,
+        from_server
+    );
+
+    Ok(())
+}
